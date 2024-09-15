a7b48042488225bb9245be928c86cf97c7bdf21a
Wade Wright <wwright@besler.com>
2024-09-15 12:57:01 -0500

    Refactor root.tsx to remove unused code and improve code organization

--- app/root.tsx
+++ app/root.tsx
@@ -44,26 +44,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
-}
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import styles from "./tailwind.css";
-
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles },
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
-
-export default function App() {
   return (
     <html lang="en">
       <head>

1fdbcca60e30f151e3b34cce029bd7269319a53f
Wade Wright <wwright@besler.com>
2024-09-16 20:56:57 -0500

    refactor: Update vite.config.ts to add alias for app directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import path from "path";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './app'),
+    },
+  },  plugins: [
     remix({
       future: {
         v3_fetcherPersist: true,
